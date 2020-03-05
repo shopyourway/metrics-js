a36378246285ff7ed5c451232263d471008dcdc1
yossi.shmueli <ysa.angel@gmail.com>
2020-03-05 20:04:44 +0200

    chore (Space): fix eslint errors

--- Space.js
+++ Space.js
@@ -1,59 +1,57 @@
 module.exports = function Space(key, reporters, errback) {
 
   function forEachReporter(func) {
-    reporters.forEach(function(reporter) {
+    reporters.forEach(reporter => {
       try {
         func(reporter);
       }
       catch (e) {
-        var errMsg = e && e.message ? e.message : 'Error in reporter';
+        const errMsg = e && e.message ? e.message : 'Error in reporter';
         (typeof errback === 'function' ? errback : console.log)(errMsg);
       }
     });
   }
 
-  this.value = function (val) {
+  this.value = val => {
     forEachReporter(reporter => reporter.value(key, val));
   };
 
-  this.increment = function (val = 1) {
+  this.increment = (val = 1) => {
     forEachReporter(reporter => reporter.increment(key, val));
   };
 
-  this.meter = function(func) {
-    if(typeof func !== 'function') {
+  this.meter = func => {
+    if (typeof func !== 'function') {
       throw new Error('must pass a function as argument');
     }
 
-    return function() {
-      var args = Array.prototype.slice.call(arguments);
-      var start = new Date();
+    return (...args) => {
+      const start = new Date();
 
-      if(isAsyncFunc(args)) {
-        var callback = args.pop();
-        args.push(function() {
-          var callbackArgs = Array.prototype.slice.call(arguments);
-          var finish = new Date();
+      if (isAsyncFunc(args)) {
+        const callback = args.pop();
+        args.push((...callbackArgs) => {
+          const finish = new Date();
           report(key, start, finish);
           callback.apply(this, callbackArgs);
         });
 
         func.apply(this, args);
       } else {
-        var result = func.apply(this, args);
-        var finish = new Date();
+        const result = func.apply(this, args);
+        const finish = new Date();
         report(key, start, finish);
         return result;
       }
     };
   };
 
-  this.space = function(nextKey) {
+  this.space = nextKey => {
     return new Space(`${key}.${nextKey}`, reporters, errback);
   };
 
   function report(key, start, finish) {
-    var duration = finish.getTime() - start.getTime();
+    const duration = finish.getTime() - start.getTime();
     forEachReporter(reporter => reporter.report(key, duration));
   }
 };
