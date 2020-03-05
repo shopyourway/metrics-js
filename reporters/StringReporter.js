b191ad1383a52a00f728c073e8d98708a5a4af39
yossi.shmueli <ysa.angel@gmail.com>
2020-03-05 20:43:04 +0200

    chore (StringReporter): fix eslint errors

--- reporters/StringReporter.js
+++ reporters/StringReporter.js
@@ -1,20 +1,20 @@
 const map = new Map();
 
 module.exports = function StringReporter(func) {
-  this.report = function(key, value) {
-    func('METRICS ' + key + ' : ' + value);
+  this.report = (key, value) => {
+    func(`METRICS ${key} : ${value}`);
   };
 
-  this.value = function(key, value) {
-    func('METRICS ' + key + ' : ' + value);
+  this.value = (key, value) => {
+    func(`METRICS ${key} : ${value}`);
   };
 
-  this.increment = function(key, value = 1) {
+  this.increment = (key, value = 1) => {
     let oldValue = map.get(key) || 0;
 
     oldValue += value;
 
     map.set(key, oldValue);
-    func('METRICS ' + key + ' : ' + oldValue);
+    func(`METRICS ${key} : ${oldValue}`);
   };
 };
