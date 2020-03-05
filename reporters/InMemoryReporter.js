d523c172723995bef65f1d2afb2470d08f5b46ad
yossi.shmueli <ysa.angel@gmail.com>
2020-03-05 20:40:35 +0200

    chore (InMemoryReporter): fix eslint errors

--- reporters/InMemoryReporter.js
+++ reporters/InMemoryReporter.js
@@ -1,20 +1,20 @@
 const map = new Map();
 
 module.exports = function InMemoryReporter(arr) {
-  this.report = function(key, value) {
-    arr.push({ key: key, value: value });
+  this.report = (key, value) => {
+    arr.push({ key, value });
   };
 
-  this.value = function(key, value) {
-    arr.push({ key: key, value: value });
+  this.value = (key, value) => {
+    arr.push({ key, value });
   };
 
-  this.increment = function(key, value = 1) {
+  this.increment = (key, value = 1) => {
     let oldValue = map.get(key) || 0;
 
     oldValue += value;
 
     map.set(key, oldValue);
-    arr.push({ key: key, value: oldValue });
+    arr.push({ key, value: oldValue });
   };
 };
