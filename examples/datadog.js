ef415c2e79137e33b897570bc88addc4184c1c15
yossi.shmueli <ysa.angel@gmail.com>
2020-05-18 20:21:04 +0300

    chore(example): add example for usage in datadog

---
+++ examples/datadog.js
@@ -0,0 +1,15 @@
+const { Metrics, DataDogReporter } = require('..');
+
+const agent = '127.0.0.1'; // Graphite server IP address
+const port = 8125; // Optional - port number. Defaults to 8125
+const spacePrefix = 'metric.test'; // Optional - prefix to all metrics spaces
+
+const dataDogReporter = new DataDogReporter({
+  host: agent,
+  port,
+  prefix: spacePrefix,
+});
+
+const metrics = new Metrics([dataDogReporter]);
+
+metrics.space('example.metrics.datadog', { tag1: 'value1', env: 'test' }).value(123);
