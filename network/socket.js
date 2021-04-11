706f8c5dbda3780bd856be41e9f36be831c5622c
ysa23 <ysa.angel@gmail.com>
2021-04-11 15:57:43 +0300

    chore(socket): add tests

--- network/socket.js
+++ network/socket.js
@@ -3,17 +3,38 @@ const dgram = require('dgram');
 module.exports = function Socket({
   port, host, buffer = true, maxBufferSize = 1000,
 }) {
+  if (!port) {
+    throw new TypeError('port is mandatory');
+  }
+  if (!host) {
+    throw new TypeError('host is mandatory');
+  }
+
   const socket = dgram.createSocket('udp4');
   socket.unref();
 
   this.send = ({ message, callback }) => {
+    if (!message) {
+      throw new TypeError('message is mandatory');
+    }
+    if (callback && typeof callback !== 'function') {
+      throw new TypeError('callback should be a function');
+    }
+
     const bytes = Buffer.from(message);
     socket.send(bytes, 0, bytes.length, port, host, err => {
       socket.close();
 
-      if (err && typeof callback === 'function') {
-        callback(err);
+      if (!callback) {
+        return;
       }
+
+      if (err) {
+        callback(err);
+        return;
+      }
+
+      callback();
     });
   };
 };
---
+++ network/socket.test.js
@@ -0,0 +1,158 @@
+const dgram = require('dgram');
+const assert = require('assert');
+const sinon = require('sinon');
+const Socket = require('./socket');
+
+describe('socket', () => {
+  let createSocketStub;
+  beforeEach(() => {
+    createSocketStub = sinon.stub(dgram, 'createSocket');
+  });
+  afterEach(() => {
+    createSocketStub.restore();
+  });
+
+  describe('constructor', () => {
+    it('should throw when port is undefined', () => {
+      stubCreateSocket();
+
+      assert.throws(() => new Socket({
+        host: '127.0.0.1',
+      }), TypeError);
+    });
+
+    it('should throw when host is undefined', () => {
+      stubCreateSocket();
+
+      assert.throws(() => new Socket({
+        port: 1234,
+      }), TypeError);
+    });
+
+    it('should create a socket', () => {
+      stubCreateSocket();
+
+      // eslint-disable-next-line no-new
+      new Socket({
+        port: 1234,
+        host: '127.0.0.1',
+      });
+
+      assert.ok(createSocketStub.calledOnce);
+    });
+
+    it('should unref socket upon creation', () => {
+      const { unref } = stubCreateSocket();
+
+      // eslint-disable-next-line no-new
+      new Socket({
+        port: 1234,
+        host: '127.0.0.1',
+      });
+
+      assert.ok(unref.calledOnce);
+    });
+  });
+
+  describe('send', () => {
+    it('should throw when message is undefined', () => {
+      stubCreateSocket();
+
+      const target = new Socket({
+        port: 1234,
+        host: '127.0.0.1',
+      });
+
+      assert.throws(() => target.send({ callback: () => {} }), TypeError);
+    });
+
+    it('should send message', () => {
+      const { send } = stubCreateSocket();
+
+      const target = new Socket({
+        port: 1234,
+        host: '127.0.0.1',
+      });
+
+      target.send({ message: 'a message from beyond' });
+
+      assert.ok(send.calledOnce);
+      const { args } = send.getCall(0);
+      assert.strictEqual(args[0].toString(), 'a message from beyond', 'message is not equal');
+      assert.strictEqual(args[1], 0, 'offset is not 0');
+      assert.strictEqual(args[2], 21, 'length is different then expected');
+      assert.strictEqual(args[3], 1234, 'port is different then expected');
+      assert.strictEqual(args[4], '127.0.0.1', 'hoat is different then expected');
+    });
+
+    it('should trigger callback when callback is specified', () => {
+      const { send } = stubCreateSocket();
+      send.callsArg(5);
+
+      const target = new Socket({
+        port: 1234,
+        host: '127.0.0.1',
+      });
+
+      const callback = sinon.spy();
+
+      target.send({ message: 'a message from beyond', callback });
+
+      assert.ok(callback.calledOnce);
+    });
+
+    it('should trigger callback with error when error from send occurs', () => {
+      const { send } = stubCreateSocket();
+      const error = new Error();
+      send.callsArgWith(5, error);
+
+      const target = new Socket({
+        port: 1234,
+        host: '127.0.0.1',
+      });
+
+      const callback = sinon.spy();
+
+      target.send({ message: 'a message from beyond', callback });
+
+      assert.ok(callback.calledOnce);
+      assert.strictEqual(callback.getCall(0).args[0], error);
+    });
+
+    it('should not throw when callback is undefined', () => {
+      const { send } = stubCreateSocket();
+      send.callsArgWith(5);
+
+      const target = new Socket({
+        port: 1234,
+        host: '127.0.0.1',
+      });
+
+      target.send({ message: 'a message from beyond' });
+
+      assert.ok(send.calledOnce);
+    });
+
+    it('should throw when callback is not a function', () => {
+      stubCreateSocket();
+
+      const target = new Socket({
+        port: 1234,
+        host: '127.0.0.1',
+      });
+
+      assert.throws(() => target.send({ message: 'a message from beyond', callback: 'not a function' }), TypeError);
+    });
+  });
+
+  function stubCreateSocket() {
+    const socketStub = {
+      send: sinon.stub(),
+      unref: sinon.stub(),
+      close: sinon.stub(),
+    };
+    createSocketStub.withArgs('udp4').returns(socketStub);
+
+    return socketStub;
+  }
+});
