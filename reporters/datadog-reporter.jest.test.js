b781869ef853bfb6eaaa409c3a8d35d68e9b4f33
ysa23 <ysa.angel@gmail.com>
2021-04-19 19:11:32 +0300

    chore(datadog reporter): convert tests to jest

--- reporters/datadog-reporter.jest.test.js
+++ reporters/datadog-reporter.jest.test.js
@@ -1,26 +1,24 @@
-const assert = require('assert');
-const sinon = require('sinon');
 const dgram = require('dgram');
 
+jest.mock('dgram');
+
+const { when } = require('jest-when');
 const { Metrics, DataDogReporter } = require('../index');
 
-let createSocketStub;
-let DateStub;
+let dateStub;
 
 describe('DataDogReporter', () => {
   describe('report', () => {
     beforeEach(() => {
-      createSocketStub = sinon.stub(dgram, 'createSocket');
-      DateStub = sinon.stub(Date, 'now');
+      dateStub = jest.spyOn(Date, 'now');
     });
     afterEach(() => {
-      createSocketStub.restore();
-      DateStub.restore();
+      dateStub.mockRestore();
     });
 
     it('should send data to DataDog in the form of "key:value|ms"', () => new Promise(done => {
       const { send } = stubCreateSocket();
-      stubDateNow(1464260419000);
+      setDate(1464260419000);
       const options = { host: '1.2.3.4', batch: false };
       const reporter = new DataDogReporter(options);
       const metrics = new Metrics([reporter]);
@@ -29,14 +27,15 @@ describe('DataDogReporter', () => {
       const wrappedFunc = metrics.space('metric.test.datadog').meter(func);
 
       wrappedFunc(1, 1, () => {
-        assert.ok(send.calledOnce);
-        const { args } = send.getCall(0);
+        expect(send).toBeCalledTimes(1);
+        const args = send.mock.calls[0];
 
         const result = splitStat(args[0].toString());
 
-        assert.equal(result[0], 'metric.test.datadog');
-        assert.ok(result[1] >= 900 && result[1] < 1500);
-        assert.equal(result[2], 'ms');
+        expect(result[0]).toEqual('metric.test.datadog');
+        expect(result[1]).toBeGreaterThanOrEqual(900);
+        expect(result[1]).toBeLessThanOrEqual(1500);
+        expect(result[2]).toEqual('ms');
         done();
       });
     }));
@@ -52,10 +51,10 @@ describe('DataDogReporter', () => {
       const wrappedFunc = metrics.space('metric.test.datadog').meter(func);
 
       wrappedFunc(1, 1, () => {
-        assert.ok(send.calledOnce);
-        const { args } = send.getCall(0);
+        expect(send).toBeCalledTimes(1);
+        const args = send.mock.calls[0];
         const result = args[3];
-        assert.equal(result, expected);
+        expect(result).toEqual(expected);
         done();
       });
     }));
@@ -71,10 +70,10 @@ describe('DataDogReporter', () => {
       const wrappedFunc = metrics.space('metric.test.datadog').meter(func);
 
       wrappedFunc(1, 1, () => {
-        assert.ok(send.calledOnce);
-        const { args } = send.getCall(0);
+        expect(send).toBeCalledTimes(1);
+        const args = send.mock.calls[0];
         const result = splitStat(args[0].toString())[0];
-        assert.equal(result, expected);
+        expect(result).toEqual(expected);
         done();
       });
     }));
@@ -90,17 +89,17 @@ describe('DataDogReporter', () => {
       const wrappedFunc = metrics.space('metric.test.datadog').meter(func);
 
       wrappedFunc(1, 1, () => {
-        assert.ok(send.calledOnce);
-        const { args } = send.getCall(0);
+        expect(send).toBeCalledTimes(1);
+        const args = send.mock.calls[0];
         const result = splitStat(args[0].toString())[0];
-        assert.equal(result, expected);
+        expect(result).toEqual(expected);
         done();
       });
     }));
 
     it('when tags are specified, should send data to DataDog in the form of "key:value|ms|#tag:value"', () => new Promise(done => {
       const { send } = stubCreateSocket();
-      stubDateNow(1464260419000);
+      setDate(1464260419000);
       const options = { host: '1.2.3.4', batch: false };
       const reporter = new DataDogReporter(options);
       const metrics = new Metrics([reporter]);
@@ -109,28 +108,22 @@ describe('DataDogReporter', () => {
       const wrappedFunc = metrics.space('metric.test.datadog', { tag1: 'value1', tag2: 'value2' }).meter(func);
 
       wrappedFunc(1, 1, () => {
-        assert.ok(send.calledOnce);
-        const { args } = send.getCall(0);
+        expect(send).toBeCalledTimes(1);
+        const args = send.mock.calls[0];
 
         const result = splitStat(args[0].toString());
 
-        assert.equal(result[0], 'metric.test.datadog');
-        assert.ok(result[1] >= 900 && result[1] < 1100);
-        assert.equal(result[2], 'ms');
-        assert.equal(result[3], '#tag1:value1,tag2:value2');
+        expect(result[0]).toEqual('metric.test.datadog');
+        expect(result[1]).toBeGreaterThanOrEqual(900);
+        expect(result[1]).toBeLessThanOrEqual(1100);
+        expect(result[2]).toEqual('ms');
+        expect(result[3]).toEqual('#tag1:value1,tag2:value2');
         done();
       });
     }));
   });
 
   describe('value', () => {
-    beforeEach(() => {
-      createSocketStub = sinon.stub(dgram, 'createSocket');
-    });
-    afterEach(() => {
-      createSocketStub.restore();
-    });
-
     it('should send data to DataDog in the form of "key:value|g"', () => {
       const { send } = stubCreateSocket();
       const options = { host: '1.2.3.4', batch: false };
@@ -139,10 +132,10 @@ describe('DataDogReporter', () => {
 
       metrics.space('metric.test.value').value(5);
 
-      const { args } = send.getCall(0);
-      assert.ok(send.calledOnce);
+      expect(send).toBeCalledTimes(1);
+      const args = send.mock.calls[0];
       const result = args[0].toString();
-      assert.equal(result, 'metric.test.value:5|g');
+      expect(result).toEqual('metric.test.value:5|g');
     });
 
     it('should use the default DataDog port if no port is provided', () => {
@@ -154,10 +147,10 @@ describe('DataDogReporter', () => {
 
       metrics.space('metric.test.value').value(5);
 
-      assert.ok(send.calledOnce);
-      const { args } = send.getCall(0);
+      expect(send).toBeCalledTimes(1);
+      const args = send.mock.calls[0];
       const result = args[3];
-      assert.equal(result, expected);
+      expect(result).toEqual(expected);
     });
 
     it('should add a valid prefix to the key when one is provided with a trailing dot', () => {
@@ -169,10 +162,10 @@ describe('DataDogReporter', () => {
 
       metrics.space('metric.test.value').value(10);
 
-      assert.ok(send.calledOnce);
-      const { args } = send.getCall(0);
+      expect(send).toBeCalledTimes(1);
+      const args = send.mock.calls[0];
       const result = args[0].toString();
-      assert.equal(result, expected);
+      expect(result).toEqual(expected);
     });
 
     it('should add a valid prefix to the key when one is provided without a trailing dot', () => {
@@ -184,10 +177,10 @@ describe('DataDogReporter', () => {
 
       metrics.space('metric.test.value').value(20);
 
-      assert.ok(send.calledOnce);
-      const { args } = send.getCall(0);
+      expect(send).toBeCalledTimes(1);
+      const args = send.mock.calls[0];
       const result = args[0].toString();
-      assert.equal(result, expected);
+      expect(result).toEqual(expected);
     });
 
     it('when tags are specified, should send data to DataDog in the form of "key:value|g|#tag:value"', () => {
@@ -198,21 +191,14 @@ describe('DataDogReporter', () => {
 
       metrics.space('metric.test.value', { tag1: 'value1', tag2: 'value2' }).value(5);
 
-      const { args } = send.getCall(0);
-      assert.ok(send.calledOnce);
+      expect(send).toBeCalledTimes(1);
+      const args = send.mock.calls[0];
       const result = args[0].toString();
-      assert.equal(result, 'metric.test.value:5|g|#tag1:value1,tag2:value2');
+      expect(result).toEqual('metric.test.value:5|g|#tag1:value1,tag2:value2');
     });
   });
 
   describe('increment', () => {
-    beforeEach(() => {
-      createSocketStub = sinon.stub(dgram, 'createSocket');
-    });
-    afterEach(() => {
-      createSocketStub.restore();
-    });
-
     it('should send data to DataDog in the form of "key:value|c"', () => {
       const { send } = stubCreateSocket();
       const options = { host: '1.2.3.4', batch: false };
@@ -221,10 +207,10 @@ describe('DataDogReporter', () => {
 
       metrics.space('metric.test.inc').increment(10);
 
-      const { args } = send.getCall(0);
-      assert.ok(send.calledOnce);
+      expect(send).toBeCalledTimes(1);
+      const args = send.mock.calls[0];
       const result = args[0].toString();
-      assert.equal(result, 'metric.test.inc:10|c');
+      expect(result).toEqual('metric.test.inc:10|c');
     });
 
     it('should use the default DataDog port if no port is provided', () => {
@@ -236,10 +222,10 @@ describe('DataDogReporter', () => {
 
       metrics.space('metric.test.inc').increment(5);
 
-      assert.ok(send.calledOnce);
-      const { args } = send.getCall(0);
+      expect(send).toBeCalledTimes(1);
+      const args = send.mock.calls[0];
       const result = args[3];
-      assert.equal(result, expected);
+      expect(result).toEqual(expected);
     });
 
     it('should add a valid prefix to the key when one is provided with a trailing dot', () => {
@@ -251,10 +237,10 @@ describe('DataDogReporter', () => {
 
       metrics.space('metric.test.inc').increment(10);
 
-      assert.ok(send.calledOnce);
-      const { args } = send.getCall(0);
+      expect(send).toBeCalledTimes(1);
+      const args = send.mock.calls[0];
       const result = args[0].toString();
-      assert.equal(result, expected);
+      expect(result).toEqual(expected);
     });
 
     it('should add a valid prefix to the key when one is provided without a trailing dot', () => {
@@ -266,10 +252,10 @@ describe('DataDogReporter', () => {
 
       metrics.space('metric.test.inc').increment(20);
 
-      assert.ok(send.calledOnce);
-      const { args } = send.getCall(0);
+      expect(send).toBeCalledTimes(1);
+      const args = send.mock.calls[0];
       const result = args[0].toString();
-      assert.equal(result, expected);
+      expect(result).toEqual(expected);
     });
 
     it('when tags are specified, should send data to DataDog in the form of "key:value|c|#tag:value"', () => {
@@ -280,10 +266,10 @@ describe('DataDogReporter', () => {
 
       metrics.space('metric.test.inc', { tag1: 'value1', tag2: 'value2' }).increment(10);
 
-      const { args } = send.getCall(0);
-      assert.ok(send.calledOnce);
+      expect(send).toBeCalledTimes(1);
+      const args = send.mock.calls[0];
       const result = args[0].toString();
-      assert.equal(result, 'metric.test.inc:10|c|#tag1:value1,tag2:value2');
+      expect(result).toEqual('metric.test.inc:10|c|#tag1:value1,tag2:value2');
     });
 
     it('when only default tags are specified, should send data to DataDog in the form of "key:value|c|#tag:value"', () => {
@@ -294,10 +280,10 @@ describe('DataDogReporter', () => {
 
       metrics.space('metric.test.inc').increment(10);
 
-      const { args } = send.getCall(0);
-      assert.ok(send.calledOnce);
+      expect(send).toBeCalledTimes(1);
+      const args = send.mock.calls[0];
       const result = args[0].toString();
-      assert.equal(result, 'metric.test.inc:10|c|#tag1:value1,tag2:value2');
+      expect(result).toEqual('metric.test.inc:10|c|#tag1:value1,tag2:value2');
     });
   });
 });
@@ -313,22 +299,24 @@ function getAsyncFunc(duration) {
 
 function stubCreateSocket() {
   const socketStub = {
-    send: sinon.spy(),
-    unref: sinon.spy(),
+    send: jest.fn(),
+    unref: jest.fn(),
   };
-  createSocketStub.withArgs('udp4').returns(socketStub);
+  when(dgram.createSocket)
+    .calledWith('udp4')
+    .mockReturnValueOnce(socketStub);
 
   return socketStub;
 }
 
-function stubDateNow(timestamp) {
+function setDate(timestamp) {
   const date = new Date(timestamp);
-  DateStub.withArgs().returns(date);
+  dateStub.mockImplementation(() => date);
 }
 
 function splitStat(str) {
   const firstPart = str.substr(0, str.indexOf(':'));
   const secondPart = str.substr(str.indexOf(':') + 1);
   const parts2 = secondPart.split('|');
-  return [firstPart, ...parts2];
+  return [firstPart, parseInt(parts2[0], 10), ...parts2.slice(1)];
 }
