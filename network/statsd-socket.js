8fe61e431b3d374773dea39c65ba2fbc0c832fef
ysa23 <ysa.angel@gmail.com>
2022-02-01 13:19:08 +0200

    chore(statsd): extract statsd reporting

--- network/statsd-socket.js
+++ network/statsd-socket.js
@@ -1,13 +1,20 @@
 const { Socket } = require('./socket');
 
+const redundantDotsRegex = new RegExp('\\.\\.+', 'g');
+
 function StatsdSocket({
-  port,
+  port = 8125,
   host,
   batch = true,
   maxBufferSize = 1000,
   flushInterval = 1000,
-  defaultTags,
+  tags: defaultTags,
+  prefix,
 }) {
+  if (defaultTags && (Array.isArray(defaultTags) || typeof defaultTags !== 'object')) throw new TypeError('tags should be an object');
+
+  const metricPrefix = typeof prefix === 'string' && prefix.length ? removeRedundantDots(`${prefix}.`) : '';
+
   const socket = new Socket({
     host, port, batch, maxBufferSize, flushInterval,
   });
@@ -15,17 +22,48 @@ function StatsdSocket({
   function send({
     key, value, type, tags, callback,
   }) {
+    validate({ name: 'key', value: key, type: 'string' });
+    validate({ name: 'value', value, type: 'number' });
+    validate({ name: 'type', value: type, type: 'string' });
+    if (tags && (Array.isArray(tags) || typeof tags !== 'object')) throw new TypeError('tags should be an object');
+    if (callback && typeof callback !== 'function') throw new TypeError('callback should be a function');
 
+    const metric = `${metricPrefix}${key}:${value}|${type}${stringifyTags(tags)}`;
+
+    socket.send({ message: metric, callback });
+  }
+
+  function validate({ name, value, type }) {
+    if (!value) throw new TypeError(`${name} is missing`);
+    // eslint-disable-next-line valid-typeof
+    if (typeof value !== type) throw new TypeError(`${name} is not a ${type}: ${value}: ${typeof value}`);
   }
 
   function close() {
+    socket.close();
+  }
 
+  function stringifyTags(tags) {
+    if (!tags && !defaultTags) {
+      return '';
+    }
+
+    const allTags = {
+      ...defaultTags,
+      ...tags,
+    };
+
+    return `|#${Object.entries(allTags).map(([key, value]) => `${key}:${value}`).join(',')}`;
   }
 
   return {
     send,
     close,
   };
+}
+
+function removeRedundantDots(str) {
+  return str.replace(redundantDotsRegex, '.');
 }
 
 module.exports = {
--- network/statsd-socket.test.js
+++ network/statsd-socket.test.js
@@ -1,5 +1,330 @@
+jest.mock('./socket', () => ({ Socket: jest.fn() }));
+const { Socket } = require('./socket');
 const { StatsdSocket } = require('./statsd-socket');
 
-jest.mock('./socket', () => {
+describe('StatsdSocket', () => {
+  describe('constructor', () => {
+    it('should create socket with given host and default values', () => {
+      // eslint-disable-next-line no-new
+      new StatsdSocket({
+        host: '127.0.0.1',
+      });
 
+      expect(Socket).toBeCalledWith({
+        host: '127.0.0.1',
+        port: 8125,
+        batch: true,
+        maxBufferSize: 1000,
+        flushInterval: 1000,
+      });
+    });
+
+    it('should create socket with given values', () => {
+      // eslint-disable-next-line no-new
+      new StatsdSocket({
+        host: '127.0.0.1',
+        port: 1234,
+        batch: false,
+        maxBufferSize: 10,
+        flushInterval: 20,
+      });
+
+      expect(Socket).toBeCalledWith({
+        host: '127.0.0.1',
+        port: 1234,
+        batch: false,
+        maxBufferSize: 10,
+        flushInterval: 20,
+      });
+    });
+
+    it.each([
+      ['string', 'strings'],
+      ['number', 1],
+      ['array', ['a', 'b']],
+    ])('should throw when default tags are %s', (title, tags) => {
+      expect(() => {
+        // eslint-disable-next-line no-new
+        new StatsdSocket({
+          host: '127.0.0.1',
+          tags,
+        });
+      }).toThrow(TypeError);
+    });
+  });
+
+  describe('close', () => {
+    it('should close socket', () => {
+      const { close } = setSocket();
+
+      const target = new StatsdSocket({
+        host: '127.0.0.1',
+      });
+
+      target.close();
+
+      expect(close).toBeCalledTimes(1);
+    });
+  });
+
+  describe('send', () => {
+    describe('validations', () => {
+      it.each([
+        ['null', null],
+        ['undefined', undefined],
+        ['empty string', ''],
+        ['number', 1],
+        ['array', ['a', 'b']],
+        ['object', { key: 'value' }],
+      ])('should throw when key is %s', (title, key) => {
+        setSocket();
+
+        const target = new StatsdSocket({
+          host: '127.0.0.1',
+        });
+
+        expect(() => target.send({
+          key,
+          value: 1.2,
+          type: 'ms',
+        })).toThrow(TypeError);
+      });
+
+      it.each([
+        ['null', null],
+        ['undefined', undefined],
+        ['string', 'no string on me'],
+        ['array', ['a', 'b']],
+        ['object', { key: 'value' }],
+      ])('should throw when value is %s', (title, value) => {
+        setSocket();
+
+        const target = new StatsdSocket({
+          host: '127.0.0.1',
+        });
+
+        expect(() => target.send({
+          key: 'space.the.final.frontier',
+          value,
+          type: 'ms',
+        })).toThrow(TypeError);
+      });
+
+      it.each([
+        ['null', null],
+        ['undefined', undefined],
+        ['empty string', ''],
+        ['number', 1],
+        ['array', ['a', 'b']],
+        ['object', { key: 'value' }],
+      ])('should throw when type is %s', (title, type) => {
+        setSocket();
+
+        const target = new StatsdSocket({
+          host: '127.0.0.1',
+        });
+
+        expect(() => target.send({
+          key: 'space.the.final.frontier',
+          value: 1.2,
+          type,
+        })).toThrow(TypeError);
+      });
+
+      it.each([
+        ['string', 'a string'],
+        ['number', 1],
+        ['array', ['a', 'b']],
+      ])('should throw when tags is %s', (title, tags) => {
+        setSocket();
+
+        const target = new StatsdSocket({
+          host: '127.0.0.1',
+        });
+
+        expect(() => target.send({
+          key: 'space.the.final.frontier',
+          value: 1.2,
+          type: 'ms',
+          tags,
+        })).toThrow(TypeError);
+      });
+
+      it.each([
+        ['string', 'a string'],
+        ['number', 1],
+        ['array', ['a', 'b']],
+        ['object', { key: 'value' }],
+      ])('should throw when callback is %s', (title, callback) => {
+        setSocket();
+
+        const target = new StatsdSocket({
+          host: '127.0.0.1',
+        });
+
+        expect(() => target.send({
+          key: 'space.the.final.frontier',
+          value: 1.2,
+          type: 'ms',
+          callback,
+        })).toThrow(TypeError);
+      });
+    });
+
+    it('should send metric', () => {
+      const { send } = setSocket();
+
+      const target = new StatsdSocket({
+        host: '127.0.0.1',
+      });
+
+      target.send({
+        key: 'space.subspace',
+        value: 1,
+        type: 'ms',
+      });
+
+      expect(send).toBeCalledWith({ message: 'space.subspace:1|ms' });
+    });
+
+    it('should send callback when defined', () => {
+      const { send } = setSocket();
+      const callback = () => {};
+
+      const target = new StatsdSocket({
+        host: '127.0.0.1',
+      });
+
+      target.send({
+        key: 'space.subspace',
+        value: 1,
+        type: 'ms',
+        callback,
+      });
+
+      expect(send).toBeCalledWith({ message: 'space.subspace:1|ms', callback });
+    });
+
+    it('should append prefix if defined', () => {
+      const { send } = setSocket();
+
+      const target = new StatsdSocket({
+        host: '127.0.0.1',
+        prefix: 'namespace.prefix.',
+      });
+
+      target.send({
+        key: 'space.subspace',
+        value: 1,
+        type: 'ms',
+      });
+
+      expect(send).toBeCalledWith({ message: 'namespace.prefix.space.subspace:1|ms' });
+    });
+
+    it('should append prefix if defined without trailing dots', () => {
+      const { send } = setSocket();
+
+      const target = new StatsdSocket({
+        host: '127.0.0.1',
+        prefix: 'namespace.prefix',
+      });
+
+      target.send({
+        key: 'space.subspace',
+        value: 1,
+        type: 'ms',
+      });
+
+      expect(send).toBeCalledWith({ message: 'namespace.prefix.space.subspace:1|ms' });
+    });
+
+    it.each([
+      ['null', null],
+      ['undefined', undefined],
+      ['empty string', ''],
+      ['number', 1],
+      ['array', ['a', 'b']],
+      ['object', { key: 'value' }],
+    ])('should ignore prefix if it is %s', (title, prefix) => {
+      const { send } = setSocket();
+
+      const target = new StatsdSocket({
+        host: '127.0.0.1',
+        prefix,
+      });
+
+      target.send({
+        key: 'space.subspace',
+        value: 1,
+        type: 'ms',
+      });
+
+      expect(send).toBeCalledWith({ message: 'space.subspace:1|ms' });
+    });
+
+    it('should append tags', () => {
+      const { send } = setSocket();
+
+      const target = new StatsdSocket({
+        host: '127.0.0.1',
+      });
+
+      target.send({
+        key: 'space.subspace',
+        value: 1,
+        type: 'ms',
+        tags: { tag1: 'value1', tag2: 'value2' },
+      });
+
+      expect(send).toBeCalledWith({ message: 'space.subspace:1|ms|#tag1:value1,tag2:value2' });
+    });
+
+    it('should append default tags', () => {
+      const { send } = setSocket();
+
+      const target = new StatsdSocket({
+        host: '127.0.0.1',
+        tags: { tag1: 'value1', tag2: 'value2' },
+      });
+
+      target.send({
+        key: 'space.subspace',
+        value: 1,
+        type: 'ms',
+      });
+
+      expect(send).toBeCalledWith({ message: 'space.subspace:1|ms|#tag1:value1,tag2:value2' });
+    });
+
+    it('should merge tags and default tags', () => {
+      const { send } = setSocket();
+
+      const target = new StatsdSocket({
+        host: '127.0.0.1',
+        tags: { tag1: 'value1', tag2: 'value2' },
+      });
+
+      target.send({
+        key: 'space.subspace',
+        value: 1,
+        type: 'ms',
+        tags: { tag2: 'overridden', tag3: 'value3' },
+      });
+
+      expect(send).toBeCalledWith({ message: 'space.subspace:1|ms|#tag1:value1,tag2:overridden,tag3:value3' });
+    });
+  });
+
+  function setSocket() {
+    const close = jest.fn();
+    const send = jest.fn();
+
+    Socket.mockImplementationOnce(() => ({ close, send }));
+
+    return {
+      close,
+      send,
+    };
+  }
 });
