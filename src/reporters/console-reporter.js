c050c4304cdf4caaf00d499d0b1791df1b46c60f
ysa23 <ysa.angel@gmail.com>
2022-02-03 12:05:31 +0200

    chore(console): refactor usage of this

--- src/reporters/console-reporter.js
+++ src/reporters/console-reporter.js
@@ -4,15 +4,21 @@ module.exports = function ConsoleReporter() {
   // eslint-disable-next-line no-console
   const stringReporter = new StringReporter(console.log);
 
-  this.report = (key, value, tags) => {
+  function report(key, value, tags) {
     stringReporter.report(key, value, tags);
-  };
+  }
 
-  this.value = (key, value, tags) => {
+  function _value(key, value, tags) {
     stringReporter.value(key, value, tags);
-  };
+  }
 
-  this.increment = (key, value = 1, tags) => {
+  function increment(key, value = 1, tags) {
     stringReporter.increment(key, value, tags);
+  }
+
+  return {
+    report,
+    value: _value,
+    increment,
   };
 };
