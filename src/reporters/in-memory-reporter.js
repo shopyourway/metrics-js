d7395e7cf4455401d0e6766a1cff9a203b2f755e
ysa23 <ysa.angel@gmail.com>
2022-02-03 12:06:56 +0200

    chore(memory): refactor usage of this

--- src/reporters/in-memory-reporter.js
+++ src/reporters/in-memory-reporter.js
@@ -1,20 +1,26 @@
 module.exports = function InMemoryReporter(arr) {
   const map = new Map();
 
-  this.report = (key, value, tags) => {
+  function report(key, value, tags) {
     arr.push({ key, value, tags });
-  };
+  }
 
-  this.value = (key, value, tags) => {
+  function _value(key, value, tags) {
     arr.push({ key, value, tags });
-  };
+  }
 
-  this.increment = (key, value = 1, tags) => {
+  function increment(key, value = 1, tags) {
     let oldValue = map.get(key) || 0;
 
     oldValue += value;
 
     map.set(key, oldValue);
     arr.push({ key, value: oldValue, tags });
+  }
+
+  return {
+    report,
+    value: _value,
+    increment,
   };
 };
