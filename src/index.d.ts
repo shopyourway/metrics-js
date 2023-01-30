bc55f9071d6780356e53df1d89f93185e867d846
ysa23 <ysa.angel@gmail.com>
2023-01-30 12:17:03 +0200

    chore(index): add declaration file

---
+++ src/index.d.ts
@@ -0,0 +1,6 @@
+export * from "./metrics";
+export * from "./reporters/console-reporter";
+export * from "./reporters/datadog-reporter";
+export * from "./reporters/graphite-reporter";
+export * from "./reporters/in-memory-reporter";
+export * from "./reporters/string-reporter";
