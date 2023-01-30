63e4af85daf50e1eab98c8a772ae7985f83e619c
ysa23 <ysa.angel@gmail.com>
2023-01-30 14:28:42 +0200

    chore: minor

--- src/reporters/in-memory-reporter.d.ts
+++ src/reporters/in-memory-reporter.d.ts
@@ -7,9 +7,8 @@ declare interface InMemoryReporterOptions {
 
 export declare class InMemoryReporter implements IReporter {
   constructor(options: InMemoryReporterOptions);
+
   increment(key: string, value?: number, tags?: Tags);
-
   report(key: string, value: number, tags?: Tags);
-
   value(key: string, value: number, tags?: Tags);
 }
