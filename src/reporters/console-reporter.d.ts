19bad89160f8534dc6ed139b9cf13c04125b5a19
ysa23 <ysa.angel@gmail.com>
2023-01-30 14:25:07 +0200

    chore: minor

--- src/reporters/console-reporter.d.ts
+++ src/reporters/console-reporter.d.ts
@@ -3,9 +3,8 @@ import {IReporter} from "../types/reporter";
 
 export declare class ConsoleReporter implements IReporter {
   constructor();
+
   report(key: string, value: number, tags?: Tags);
-
   value(key: string, value: number, tags?: Tags);
-
   increment(key: string, value?: number, tags?: Tags);
 }
